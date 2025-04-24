d16b84fa3638d82cfe197056f413b600552b52ae
P4k5u <44562571+P4k5u@users.noreply.github.com>
2025-04-24 15:18:35 +0800

    Initial commit

---
+++ src/main/java/boonboom/BoonboomApplication.java
@@ -0,0 +1,13 @@
+package boonboom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoonboomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoonboomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/boonboom/BoonboomApplicationTests.java
@@ -0,0 +1,13 @@
+package boonboom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoonboomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
